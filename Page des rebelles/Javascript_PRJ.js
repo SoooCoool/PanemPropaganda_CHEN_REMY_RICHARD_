e6482995674ee485c1c43ffc5680ce661d915b3f
SoooCoool <62948581+SoooCoool@users.noreply.github.com>
2020-06-15 00:34:43 +0200

    Add files via upload

---
+++ Page des rebelles/Javascript_PRJ.js
@@ -0,0 +1,14 @@
+function play()
+{
+    var audio = new Audio("Page des rebelles/hanging_tree.mp3");
+    audio.play();
+}
+
+function cool_button()
+{
+    var audio = new Audio("Page des rebelles/hanging_tree.mp3");
+    audio.play();
+    window.open("Page des rebelles/Page_rebelles.html");
+    document.getElementById("ans_").innerHTML = " ";
+    document.getElementById("button_d").innerHTML = " ";
+}
